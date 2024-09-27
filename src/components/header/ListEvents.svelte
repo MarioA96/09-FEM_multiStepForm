149b0ccf1869b544109fe9e7d325ef3347696289
MarioA96 <marioaquirozs@gmail.com>
2024-09-27 14:48:09 -0600

    Se agregaron elementos estaticos del proyecto
    Se agregaron fuentes, imágenes y fav-icons.

---
+++ src/components/header/ListEvents.svelte
@@ -0,0 +1,86 @@
+<script lang="ts">
+
+    import { formNumProcess } from '../../stores/form.store';
+
+    import { cva } from '../../../styled-system/css';
+
+    const ulStyles = cva({
+        base: {
+            display: 'flex', 
+            flexDirection: 'row',
+            '& li': {
+                listStyle: 'none',
+                width: '30px',
+                height: '30px',
+                margin: '10px',
+                borderRadius: '50%',
+                border: '1px solid white',
+                textAlign: 'center',
+                alignContent: 'center',
+                fontFamily: 'ubuntu_bold',
+                fontSize: '14px'
+            },
+            '& [id$=_unchecked]':{
+                color: 'white',
+                '&:hover': {
+                    backgroundColor: 'hsla(229, 57%, 89%, 0.5)',
+                    color: 'hsl(213, 96%, 18%)',
+                }
+            },
+            '& [id$=_checked]': {
+                backgroundColor: 'hsl(229, 57%, 89%)',
+                color: 'hsl(213, 96%, 18%)'
+            },
+            '& button': {
+                cursor: 'pointer',
+                width: '100%',
+                height: '100%',
+            }
+        }
+    })
+
+</script>
+
+<ul class={ulStyles({})}>
+
+    {#if $formNumProcess === 1}
+        <li id="list_1_checked">1</li>
+    {:else}
+        <li id="list_1_unchecked" >
+            <button on:click={ () => formNumProcess.set(1) }>
+                1
+            </button>
+        </li>
+    {/if}
+
+    {#if $formNumProcess === 2}
+        <li id="list_2_checked">2</li>
+    {:else}
+        <li id="list_2_unchecked">
+            <button on:click={ () => formNumProcess.set(2) }>
+                2
+            </button>
+        </li>
+    {/if}
+
+    {#if $formNumProcess === 3}
+        <li id="list_3_checked">3</li>
+    {:else}
+        <li id="list_3_unchecked">
+            <button on:click={ () => formNumProcess.set(3) }>
+                3
+            </button>
+        </li>
+    {/if}
+
+    {#if $formNumProcess === 4}
+        <li id="list_4_checked">4</li>
+    {:else}
+        <li id="list_4_unchecked">
+            <button on:click={ () => formNumProcess.set(4) }>
+                4
+            </button>
+        </li>
+    {/if}
+
+</ul>
