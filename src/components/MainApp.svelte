77a30229563a61189c7de8d3bc32fba896393652
MarioA96 <marioaquirozs@gmail.com>
2024-09-30 20:04:30 -0600

    Se agrego el componente padre principal MainApp

---
+++ src/components/MainApp.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+
+    import CardPersonalInfo from "./card/CardPersonalInfo.svelte"; 
+    import CardSelectPlan from "./card/CardSelectPlan.svelte"; 
+
+</script>
+
+<!--
+    <CardPersonalInfo />
+-->
+<CardSelectPlan />
