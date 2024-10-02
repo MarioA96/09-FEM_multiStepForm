b6485319acf266fdd6b4ab7ced37e69261829324
MarioA96 <marioaquirozs@gmail.com>
2024-10-02 16:24:45 -0600

    Se agrego la sección del Header del Card
    Se agrego para ui component el componente del Card la sección dedicada Card_Header.svelte

---
+++ src/ui/components/Card_Header.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import { css } from "../../../styled-system/css";
+</script>
+
+<div id="card_header" 
+    class={
+        css({
+            display: 'flex',
+            width: '100%',
+            height: 'fit-content',
+            padding: '25px 20px 20px 20px'
+        })
+    }
+>
+    <slot />
+</div>
