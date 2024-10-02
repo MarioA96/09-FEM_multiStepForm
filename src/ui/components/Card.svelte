995a834bffc9b18fa39f1d2186db35ead8cebd23
MarioA96 <marioaquirozs@gmail.com>
2024-10-02 16:20:23 -0600

    Se agrego el ui/componente Card.svelte
    Se creo un componente general para el manejo de Cards a través de la aplicación.

---
+++ src/ui/components/Card.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+    import { cx, css } from "../../../styled-system/css"; 
+</script>
+
+
+<div id="main_box" 
+    class={ 
+        cx( 
+            css({
+                display: 'flex',
+                position: 'absolute', 
+                zIndex: 2,
+                width: { 
+                    xsm: '100%', 
+                    lg: '20%' 
+                }, 
+                height: { 
+                    xsm: 'fit-content', 
+                    lg: '100%' 
+                },
+                marginTop: { 
+                    xsm: '100px', 
+                    lg: '400px' 
+                },
+                justifyItems: 'center',
+                justifyContent: 'center',
+            })
+        )
+    }
+>
+
+    <div id="card_box" 
+        class={
+            css({
+                display: 'flex',
+                flexDirection: 'column',
+                width: '90%',
+                height: 'fit-content',
+                bg: 'white',
+                borderRadius: '10px',
+            })
+        }	
+    >
+        
+        <slot />
+
+    </div>
+
+</div>
