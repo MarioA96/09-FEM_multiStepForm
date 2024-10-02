a76fcbe4b53548f2c6b2e25a9b3695c2c39f360b
MarioA96 <marioaquirozs@gmail.com>
2024-10-02 16:22:46 -0600

    Se agrego la sección de Descripción para Card
    Se agregó al ui component del Card su sección dedicada Card_Description.svelte.

---
+++ src/ui/components/Card_Description.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import { css } from "../../../styled-system/css";
+</script>
+
+<div id="card_description" 
+    class={
+        css({
+            display: 'flex',
+            width: '100%',
+            height: 'fit-content',
+            padding: '0px 20px 25px 20px'
+        })
+    }
+>
+    <slot />
+</div>
