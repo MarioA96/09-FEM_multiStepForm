5e310a4558a1d3b093d6fc70d16de5251239239e
MarioA96 <marioaquirozs@gmail.com>
2024-10-08 15:56:06 -0600

    Se establecieron las rutas basadas en el formNumProcess

--- src/components/MainApp.svelte
+++ src/components/MainApp.svelte
@@ -1,11 +1,23 @@
 <script lang="ts">
+    import { formNumProcess } from '../stores/form.store';
 
     import CardPersonalInfo from "./card/CardPersonalInfo.svelte"; 
     import CardSelectPlan from "./card/CardSelectPlan.svelte"; 
+    import CardPickAddOns from "./card/CardPickAddOns.svelte"; 
 
 </script>
 
+{#if $formNumProcess === 1}
+    <CardPersonalInfo />
+{:else if $formNumProcess === 2}
+    <CardSelectPlan />
+{:else if $formNumProcess === 3}
+    <CardPickAddOns />
 <!--
+{:else if $formNumProcess === 4}
+    <CardPayment />
+{:else}
     <CardPersonalInfo />
 -->
-<CardSelectPlan />
+{/if}
+    
