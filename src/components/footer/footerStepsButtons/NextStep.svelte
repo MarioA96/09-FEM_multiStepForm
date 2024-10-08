f831e9f8945907981a9a6ffd41852d49f5204b23
MarioA96 <marioaquirozs@gmail.com>
2024-10-08 15:55:24 -0600

    Se agrego el componente botón NextStep

---
+++ src/components/footer/footerStepsButtons/NextStep.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    import { gridItem } from "../../../../styled-system/patterns";
+    import { css } from "../../../../styled-system/css";
+</script>
+
+
+
+<div class={ gridItem({ colStart: 9, colEnd: 12 }) }>
+    <button 
+        class={ 
+            css({ 
+                width: '100%', 
+                height: 'fit-content', 
+                padding: '15px 5px', 
+                bg: 'hsl(213, 96%, 18%)', 
+                color: 'white', 
+                fontFamily: 'ubuntu_bold',
+                fontSize: '13px', 
+                borderRadius: '5px', 
+                cursor: 'pointer',
+                '_hover': {
+                    backgroundColor: 'gray.500'
+                }
+            }) 
+        }
+    >
+        Next Step
+    </button>
+</div>
