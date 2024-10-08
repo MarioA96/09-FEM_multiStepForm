8fa914b760744433d8449a6f69174449a7145931
MarioA96 <marioaquirozs@gmail.com>
2024-10-08 15:54:57 -0600

    Se agrego el componente botón BackStep

---
+++ src/components/footer/footerStepsButtons/BackStep.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    import { formNumProcess } from "../../../stores/form.store"
+
+    import { gridItem } from "../../../../styled-system/patterns";
+    import { cva } from "../../../../styled-system/css";
+
+    const ButtonStyles = cva({
+        base: {
+            width: '100%', 
+            height: 'fit-content', 
+            padding: '15px 5px', 
+            bg: 'gray.100', 
+            color: 'hsl(231, 11%, 63%)',
+            fontFamily: 'ubuntu_bold', 
+            fontSize: '13px',
+            borderRadius: '5px', 
+            cursor: 'pointer',
+            '_hover': {
+                border: '1px solid hsl(231, 11%, 63%)'
+            }
+        }
+    });
+</script>
+
+{#if $formNumProcess >= 2}
+    <div class={ gridItem({ colStart: 2, colEnd: 5 }) }>
+        <button id="button_back" class={ButtonStyles({})} >
+            Go Back
+        </button>
+    </div>
+{/if}
