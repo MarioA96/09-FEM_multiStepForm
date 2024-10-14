dab9b2590af13d08d6c4ae7cc65f30486dcd21ce
MarioA96 <marioaquirozs@gmail.com>
2024-10-14 17:05:43 -0600

    Se simplifico la funcionalidad del componente
    Ahora gran parte de la funcionalidad del muestreo de los botones se hace mediante un ciclo y basada en el estado del componente y el store

--- src/components/card/sections/cardPlan/PlanTypesSection.svelte
+++ src/components/card/sections/cardPlan/PlanTypesSection.svelte
@@ -1,76 +1,115 @@
 <script lang="ts">
     import { planBilling, planType } from '../../../../stores/form.store';
+    
+    import type { PlanType } from '../../../../interfaces/PlanType.interface';
 
     import { boxSelectPlanStyles } from '../../../../styles/cardPlan/selectPlanStyles';
     import { cx } from "../../../../../styled-system/css";
     import { grid, gridItem } from '../../../../../styled-system/patterns';
 
+    
+    const handleSelectPlan = (planName: keyof PlanType) => {
 
-    const handleSelectPlan = (type: string) => {
-        planType.set(type);
+        console.log(planName);
+
+        switch (planName) {
+            case 'Arcade':
+                planType.set({ 
+                    Arcade: { ...planType.get().Arcade, active: true },
+
+                    Advanced: { ...planType.get().Advanced, active: false },
+                    Pro: { ...planType.get().Pro, active: false },
+                    Default: { ...planType.get().Default, active: false }
+                });
+                console.log(planType.get());
+                break;
+            case 'Advanced':
+                planType.set({ 
+                    Advanced: { ...planType.get().Advanced, active: true },
+
+                    Arcade: { ...planType.get().Arcade, active: false },
+                    Pro: { ...planType.get().Pro, active: false },
+                    Default: { ...planType.get().Default, active: false }
+                });
+                console.log(planType.get());
+                break;
+            case 'Pro':
+                planType.set({ 
+                    Pro: { ...planType.get().Pro, active: true },
+
+                    Arcade: { ...planType.get().Arcade, active: false },
+                    Advanced: { ...planType.get().Advanced, active: false },
+                    Default: { ...planType.get().Default, active: false }
+                });
+                console.log(planType.get());
+                break;
+            default:
+                break;
+        }
+
     }
 
+    type ButtonsType = {
+        id: "arcade" | "advanced" | "pro";
+        title: keyof PlanType;
+        desc: string;
+        imgSrc: string;
+        monthlyPrice: number;
+        yearlyPrice: number;
+    };
+
+    const initialButtonsAvailable: ButtonsType[] = [
+        { 
+            id: 'arcade', 
+            title: 'Arcade', 
+            desc: 'Access to multiplayer games',
+            imgSrc: '/assets/images/icon-arcade.svg',
+            monthlyPrice: planType.get().Arcade.monthlyPrice,
+            yearlyPrice: planType.get().Arcade.yearlyPrice,
+        },
+        { 
+            id: 'advanced', 
+            title: 'Advanced', 
+            desc: 'Extra 1TB of cloud save',
+            imgSrc: '/assets/images/icon-advanced.svg',
+            monthlyPrice: planType.get().Advanced.monthlyPrice,
+            yearlyPrice: planType.get().Advanced.yearlyPrice,
+        },
+        { 
+            id: 'pro', 
+            title: 'Pro', 
+            desc: 'Custom theme on your profile',
+            imgSrc: '/assets/images/icon-pro.svg',
+            monthlyPrice: planType.get().Pro.monthlyPrice,
+            yearlyPrice: planType.get().Pro.yearlyPrice,
+        }
+    ]
 </script>
 
 <div id="box_selectPlan_monthly" class={ boxSelectPlanStyles({}) }>
-                    
-    <button id="box_selectPlan_type_arcade" 
-        class={ grid({ columns: 8 })}
-        on:click={ () => {handleSelectPlan('arcade')} }
-    >
-        <div id="box_icon_plan_arcade" class={ gridItem({ colSpan: 2 }) }>
-            <img src="/assets/images/icon-arcade.svg" alt="arcade-icon">
-        </div>
-        <div id="box_info_plan_arcade" class={ cx( gridItem({ colSpan: 4 }) )}>
-            <div id="info_title_arcade">Arcade</div>
+                
+    {#each initialButtonsAvailable as button, index}
 
-            {#if $planBilling === 'monthly'}
-                <div id="info_price_arcade">$9/mo</div>
-            {:else if $planBilling === 'yearly'}
-                <div id="info_price_arcade">$90/yr</div>
-            {/if}
+        <button id={`box_selectPlan_type_${button.id}`} 
+            class={ grid({ columns: 8 })}
+            on:click={ () => {handleSelectPlan(button.title)} }
+        >
+            <div id={`box_icon_plan_${button.id}`} class={ gridItem({ colSpan: 2 }) }>
+                <img src={`${button.imgSrc}`} alt={`${button.id}-icon`}>
+            </div>
+            <div id={`box_info_plan_${button.id}`} class={ cx( gridItem({ colSpan: 4 }) )}>
+                <div id={`info_title_${button.id}`}>{button.title}</div>
 
-        </div>
-    </button>
+                {#if $planBilling === 'monthly'}
+                    <div id={`info_price_${button.id}`}>${button.monthlyPrice}/mo</div>
+                {:else if $planBilling === 'yearly'}
+                    <div id={`info_price_${button.id}`}>${button.yearlyPrice}/yr</div>
+                {/if}
 
-    <button id="box_selectPlan_type_advanced"
-        class={ grid({ columns: 8 })}
-        on:click={ () => {handleSelectPlan('advanced')} }
-    >
-        <div id="box_icon_plan_advanced" class={ gridItem({ colSpan: 2 }) }>
-            <img src="/assets/images/icon-advanced.svg" alt="advanced-icon">
-        </div>
-        <div id="box_info_plan_advanced" class={ cx( gridItem({ colSpan: 4 }) )}>
+            </div>
+        </button>
 
-            {#if $planBilling === 'monthly'}
-                <div id="info_title_advanced">Advanced</div>
-                <div id="info_price_advanced">$12/mo</div>
-            {:else if $planBilling === 'yearly'}
-                <div id="info_title_advanced">Advanced</div>
-                <div id="info_price_advanced">$120/yr</div>
-            {/if}
-
-        </div>
-    </button>
-
-    <button id="box_selectPlan_type_pro"
-        class={ grid({ columns: 8 })}
-        on:click={ () => {handleSelectPlan('pro')} }
-    >
-        <div id="box_icon_plan_pro" class={ gridItem({ colSpan: 2 }) }>
-            <img src="/assets/images/icon-pro.svg" alt="pro-icon">
-        </div>
-        <div id="box_info_plan_pro" class={ cx( gridItem({ colSpan: 4 }) )}>
-
-            {#if $planBilling === 'monthly'}
-                <div id="info_title_pro">Pro</div>
-                <div id="info_price_pro">$15/mo</div>
-            {:else if $planBilling === 'yearly'}
-                <div id="info_title_pro">Pro</div>
-                <div id="info_price_pro">$150/yr</div>
-            {/if}
-
-        </div>
-    </button>
+    {/each}
+    
 
 </div>
