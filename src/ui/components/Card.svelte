e40bc71064a083c878af97946037e624c73016e2
MarioA96 <marioaquirozs@gmail.com>
2024-10-15 15:02:41 -0600

    Se corrigieron los estilos
    Se establecieron los casos en el componente para que este sea responsivo.

--- src/ui/components/Card.svelte
+++ src/ui/components/Card.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
     import { cx, css } from "../../../styled-system/css"; 
+  import BackStep from "../../components/footer/footerStepsButtons/BackStep.svelte";
+  import ConfirmStep from "../../components/footer/footerStepsButtons/ConfirmStep.svelte";
+  import NextStep from "../../components/footer/footerStepsButtons/NextStep.svelte";
+  import FooterStepsLayout from "../../components/footer/FooterStepsLayout.svelte";
 </script>
 
 
@@ -8,19 +12,25 @@
         cx( 
             css({
                 display: 'flex',
-                position: 'absolute', 
-                zIndex: 2,
+                position: {
+                    xsm: 'absolute',
+                    lg: 'relative'
+                }, 
+                zIndex: {
+                    xsm: 2, 
+                    lg: 0
+                },
                 width: { 
-                    xsm: '100%', 
-                    lg: '20%' 
+                    xsm: '95%', 
+                    lg: '100%' 
                 }, 
                 height: { 
                     xsm: 'fit-content', 
                     lg: '100%' 
                 },
                 marginTop: { 
-                    xsm: '100px', 
-                    lg: '400px' 
+                    xsm: '-50px', 
+                    lg: '0px' 
                 },
                 justifyItems: 'center',
                 justifyContent: 'center',
@@ -34,7 +44,10 @@
             css({
                 display: 'flex',
                 flexDirection: 'column',
-                width: '90%',
+                width: { 
+                    xsm: '90%', 
+                    lg: '100%' 
+                },
                 height: 'fit-content',
                 bg: 'white',
                 borderRadius: '10px',
@@ -43,6 +56,12 @@
     >
         
         <slot />
+
+        <FooterStepsLayout>
+            <BackStep slot="button_back"/>
+            <NextStep slot="button_next"/>
+            <ConfirmStep slot="button_confirm"/>
+        </FooterStepsLayout>
 
     </div>
 
