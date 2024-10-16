95f7aef71c81f29ec6fe5dfe877b9f174f756fa8
MarioA96 <marioaquirozs@gmail.com>
2024-10-16 14:49:24 -0600

    Se actualizo el componente
    Se removieron dependencias innecesarias, se limpio el código de los estilos para manejarlo en un archivo independiente formStyles.ts, se estilizo el código para el SEO de los inputs, se agrego el componente botones de navegación entre cartas.

--- src/components/card/CardPersonalInfo.svelte
+++ src/components/card/CardPersonalInfo.svelte
@@ -1,44 +1,22 @@
 <script lang="ts">
     import { formNumProcess } from '../../stores/form.store';
 
-    import { cva, css } from "../../../styled-system/css";
+    import { css } from "../../../styled-system/css";
     
     import Card from "../../ui/components/Card.svelte"; 
     import Card_Header from "../../ui/components/Card_Header.svelte";
     import Card_Description from "../../ui/components/Card_Description.svelte";
     import Card_Body from "../../ui/components/Card_Body.svelte";
-   
 
+    import FooterStepsLayout from '../footer/FooterStepsLayout.svelte';
+    import BackStep from '../footer/footerStepsButtons/BackStep.svelte';
+    import NextStep from '../footer/footerStepsButtons/NextStep.svelte';
+    import ConfirmStep from '../footer/footerStepsButtons/ConfirmStep.svelte';
+
+    import { FormStyles } from '../../styles/cardPersonalInfo/formStyles';
+   
     $: formNumProcess.set(1);
 
-
-    const FormStyles = cva({
-        base: {
-            display: 'flex',
-            flexDirection: 'column',
-            width: '100%',
-            height: 'fit-content',
-            '& div[id^=box_personalInfo]':{
-                display: 'flex', 
-                flexDirection: 'column',
-                '& label': {
-                    fontFamily: 'ubuntu_regular',
-                    fontSize: '14px',
-                    fontWeight: '600',
-                    color: 'blue.950',
-                    marginBottom: '2px'
-                },
-                '& input': {
-                    fontFamily: 'ubuntu_regular',
-                    height: '40px',
-                    border: '0.5px solid gray',
-                    borderRadius: '5px',
-                    padding: '0px 15px',
-                    marginBottom: '1rem'
-                }
-            }
-        }
-    });
 </script>
 
 <Card>
@@ -75,19 +53,31 @@
         <form id="form_personalInfo" class={FormStyles({})}>
 
             <div id="box_personalInfo_name">
-                <label for="fpersonalInfo_name">Name</label>
-                <input type="text" name="fpersonalInfo_name" placeholder="e.g. Stephen King" required/>
+                <label for="fpersonalInfo_name">
+                    Name
+                </label>
+                <input type="text" name="fpersonalInfo_name" id="fpersonalInfo_name" placeholder="e.g. Stephen King" required/>
             </div>
             <div id="box_personalInfo_email">
-                <label for="fpersonalInfo_email">Email Address</label>
-                <input type="email" name="fpersonalInfo_email" placeholder="e.g. stephenking@lorem.com" required/>
+                <label for="fpersonalInfo_email">
+                    Email Address
+                </label>
+                <input type="email" name="fpersonalInfo_email" id="fpersonalInfo_email" placeholder="e.g. stephenking@lorem.com" required/>
             </div>
             <div id="box_personalInfo_phoneNumber">
-                <label for="fpersonalInfo_phoneNumber">Phone Number</label>
-                <input type="text" name="fpersonalInfo_phoneNumber" placeholder="e.g. +1 234 567 890" required/>
+                <label for="fpersonalInfo_phoneNumber">
+                    Phone Number
+                </label>
+                <input type="text" name="fpersonalInfo_phoneNumber" id="fpersonalInfo_phoneNumber" placeholder="e.g. +1 234 567 890" required/>
             </div>
 
         </form>
 
     </Card_Body>
+
+    <FooterStepsLayout>
+        <BackStep slot="button_back"/>
+        <NextStep slot="button_next"/>
+        <ConfirmStep slot="button_confirm"/>
+    </FooterStepsLayout>
 </Card>
