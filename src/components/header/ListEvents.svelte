73dadba7caadb6f9afb63cd950d1bf750f701e7b
MarioA96 <marioaquirozs@gmail.com>
2024-10-21 15:26:42 -0600

    Se realizo refactorización del componente
    Se separaron los estilos del componente y agrego condición dinámica para los elementos listados así como su estado basado en el numero de proceso actual.

--- src/components/header/ListEvents.svelte
+++ src/components/header/ListEvents.svelte
@@ -2,85 +2,57 @@
 
     import { formNumProcess } from '../../stores/form.store';
 
-    import { cva } from '../../../styled-system/css';
+    import { css } from '../../../styled-system/css';
+    import { grid, gridItem } from '../../../styled-system/patterns';
+    import { ulStyles } from '../../styles/header/ListEvents.styles';
 
-    const ulStyles = cva({
-        base: {
-            display: 'flex', 
-            flexDirection: 'row',
-            '& li': {
-                listStyle: 'none',
-                width: '30px',
-                height: '30px',
-                margin: '10px',
-                borderRadius: '50%',
-                border: '1px solid white',
-                textAlign: 'center',
-                alignContent: 'center',
-                fontFamily: 'ubuntu_bold',
-                fontSize: '14px'
-            },
-            '& [id$=_unchecked]':{
-                color: 'white',
-                '&:hover': {
-                    backgroundColor: 'hsla(229, 57%, 89%, 0.5)',
-                    color: 'hsl(213, 96%, 18%)',
-                }
-            },
-            '& [id$=_checked]': {
-                backgroundColor: 'hsl(229, 57%, 89%)',
-                color: 'hsl(213, 96%, 18%)'
-            },
-            '& button': {
-                cursor: 'pointer',
-                width: '100%',
-                height: '100%',
-            }
-        }
-    })
+    type Step = {
+        id: number;
+        info: string;
+    }
+
+    const arrayOfSteps: Step[]  = [
+        { id: 1, info: 'YOUR INFO' },
+        { id: 2, info: 'SELECT PLAN' },
+        { id: 3, info: 'ADD-ONS' },
+        { id: 4, info: 'SUMMARY' }
+    ];
+
 
 </script>
 
 <ul class={ulStyles({})}>
 
-    {#if $formNumProcess === 1}
-        <li id="list_1_checked">1</li>
-    {:else}
-        <li id="list_1_unchecked" >
-            <button on:click={ () => formNumProcess.set(1) }>
-                1
-            </button>
-        </li>
-    {/if}
-
-    {#if $formNumProcess === 2}
-        <li id="list_2_checked">2</li>
-    {:else}
-        <li id="list_2_unchecked">
-            <button on:click={ () => formNumProcess.set(2) }>
-                2
-            </button>
-        </li>
-    {/if}
-
-    {#if $formNumProcess === 3}
-        <li id="list_3_checked">3</li>
-    {:else}
-        <li id="list_3_unchecked">
-            <button on:click={ () => formNumProcess.set(3) }>
-                3
-            </button>
-        </li>
-    {/if}
-
-    {#if $formNumProcess === 4}
-        <li id="list_4_checked">4</li>
-    {:else}
-        <li id="list_4_unchecked">
-            <button on:click={ () => formNumProcess.set(4) }>
-                4
-            </button>
-        </li>
-    {/if}
+    {#each arrayOfSteps as step, index}
+        {#if $formNumProcess === index + 1}
+            <li id="list_{step.id}_checked" class={grid({columns: 12})}>
+                <button id="{step.id}_button_checked" class={gridItem({colSpan: 2})} disabled={($formNumProcess === 5 || $formNumProcess === 1) ? true : false}>
+                    {index + 1}
+                </button>
+                <p id="{step.id}_description_checked" class={ gridItem({colSpan: 10}) }>
+                    <span id="{step.id}_description_title" class={css({ display: 'flex' })}>
+                        STEP {step.id}
+                    </span>
+                    <span id="{step.id}_description_info" class={css({ display: 'flex' })}>
+                        {step.info}
+                    </span>
+                </p>
+            </li>
+        {:else}
+            <li id="list_{step.id}_unchecked" class={grid({columns: 12})}>
+                <button id="{step.id}_button_unchecked" on:click={ () => formNumProcess.set(index + 1) } class={gridItem({colSpan: 2})} disabled={ ($formNumProcess === 5 || $formNumProcess === 1) ? true : false}>
+                    {index + 1}
+                </button>
+                <p id="{step.id}_description_unchecked" class={ gridItem({colSpan: 10}) }>
+                    <span id="{step.id}_description_title" class={css({ display: 'flex' })}>
+                        STEP {step.id}
+                    </span>
+                    <span id="{step.id}_description_info" class={css({ display: 'flex' })}>
+                        {step.info}
+                    </span>
+                </p>
+            </li>
+        {/if}
+    {/each}
 
 </ul>
