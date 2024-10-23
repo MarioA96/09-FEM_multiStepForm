50955950be6fead0ce47ac3543739639f23cee49
MarioA96 <marioaquirozs@gmail.com>
2024-10-22 19:46:48 -0600

    Se agregaron características para el deploy.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()]
+  integrations: [svelte()],
+  site: 'https://marioa96.github.io',
+  base: '09-FEM_multiStepForm',
 });
