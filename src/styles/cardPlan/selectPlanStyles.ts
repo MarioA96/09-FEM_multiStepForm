76dd4542cafa4223a489db94b266bdb05a901c35
MarioA96 <marioaquirozs@gmail.com>
2024-10-23 15:07:13 -0600

    Se actualizo cardPlan/selectPlanStyles
    Se agregaron estilos para incorporar un modelo responsivo.

--- src/styles/cardPlan/selectPlanStyles.ts
+++ src/styles/cardPlan/selectPlanStyles.ts
@@ -3,25 +3,48 @@ import { cva } from "../../../styled-system/css";
 
 export const boxSelectPlanStyles = cva({
     base: {
+        display: 'flex',
+        flexDirection: {
+            xsm: 'column',
+            lg: 'row',
+        },
         '& button[id^=box_selectPlan_type]':{
-            width: '100%',
-            height: '70px',
+            display: 'flex',
+            flexDirection: {
+                xsm: 'row',
+                lg: 'column',
+            },
+            width: {
+                xsm: '100%',
+                lg: '90%',
+            },
+            height: {
+                xsm: '70px',
+                lg: '180px',
+            },
             border: '1px solid hsl(229, 24%, 87%)',
             borderRadius: '8px',
-            alignItems: 'center',
-            justifyContent: 'center',
+            alignItems: {
+                xsm: 'center',
+                lg: 'start',
+            },
+            justifyContent: {
+                xsm: 'left',
+                lg: 'left',
+            },
             padding: '0px 5px 0px 15px',
-            marginBottom: '10px',
-            '_focus': {
-                bg: 'blue.50',
+            margin: {
+                xsm: '0px 0px 10px 0px',
+                lg: '0px 15px 0px 0px',
+            },
+            '_hover': {
                 cursor: 'pointer',
-                border: '1px solid hsl(213, 96%, 18%)',
             },
             '& div[id^=box_icon_plan]':{
                 display: 'flex', 
                 height: '60%', 
                 alignContent: 'center', 
-                justifyContent: 'center'
+                justifyContent: 'center',
             },
             '& div[id^=box_info_plan]':{
                 height: '70%',
