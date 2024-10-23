5bee34cc5864f2ac847758f85cb6c037b2e16996
MarioA96 <marioaquirozs@gmail.com>
2024-10-22 19:49:49 -0600

    Se formateo el componente para la responsavidad.
    Se estableció una jerarquía superior en el orden semántico de la estructura.

--- src/components/MainApp.svelte
+++ src/components/MainApp.svelte
@@ -4,20 +4,42 @@
     import CardPersonalInfo from "./card/CardPersonalInfo.svelte"; 
     import CardSelectPlan from "./card/CardSelectPlan.svelte"; 
     import CardPickAddOns from "./card/CardPickAddOns.svelte"; 
+    import CardPayment from "./card/CardPayment.svelte"; 
+    import CardConfirmation from './card/CardConfirmation.svelte';
+    
+    import { css } from '../../styled-system/css';
 
 </script>
 
-{#if $formNumProcess === 1}
-    <CardPersonalInfo />
-{:else if $formNumProcess === 2}
-    <CardSelectPlan />
-{:else if $formNumProcess === 3}
-    <CardPickAddOns />
-<!--
-{:else if $formNumProcess === 4}
-    <CardPayment />
-{:else}
-    <CardPersonalInfo />
--->
-{/if}
+<div 
+    class={
+        css({ 
+            display: { 
+                xsm: 'flex', 
+                lg: 'flex' 
+            },
+            width: {
+                xsm: '100%', 
+                lg: '65%' 
+            },
+            height: {
+                xsm: 'fit-content', 
+                lg: '100%' 
+            },
+            justifyContent: 'center',
+        })
+    }
+>
+    {#if $formNumProcess === 1}
+        <CardPersonalInfo />
+    {:else if $formNumProcess === 2}
+        <CardSelectPlan />
+    {:else if $formNumProcess === 3}
+        <CardPickAddOns />
+    {:else if $formNumProcess === 4}
+        <CardPayment />
+    {:else if $formNumProcess === 5}
+        <CardConfirmation />
+    {/if}
+</div>
     
